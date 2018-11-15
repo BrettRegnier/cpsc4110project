c3f51b08bac9c4b547078410d1e75aa35d174eef
Daylend <drkcelestial@gmail.com>
2018-11-15 15:51:53 -0700

    Added Add function for complex matrices

--- src/ComplexMatrix.cc
+++ src/ComplexMatrix.cc
@@ -1,1 +1,14 @@
 #include "ComplexMatrix.hh"
+
+using namespace ComplexSpace;
+using namespace std;
+
+ComplexMatrix ComplexMatrix::Add(ComplexMatrix A)
+{
+    vector<ComplexVector> newMatrix = vector<ComplexVector>();
+    for(int i = 0; i < Matrix.size; i++)
+    {
+        ComplexVector added = Matrix[i].Add(A.Matrix[i]);
+        newMatrix.push_back(added);
+    }
+}
