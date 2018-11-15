f2f3a78aff618b18c72a47b78477e804241bc4e5
Brett Regnier <bretternestregnier@gmail.com>
2018-11-15 13:50:39 -0700

    Modulus and Conjugate created

--- src/ComplexNumber.cc
+++ src/ComplexNumber.cc
@@ -1,1 +1,13 @@
 #include "ComplexNumber.hh"
+
+using namespace ComplexSpace;
+
+float ComplexNumber::Modulus()
+{
+    return sqrt(pow(real, 2.0) + pow(imaginary, 2.0));
+}
+
+ComplexNumber ComplexNumber::Conjugate()
+{
+    imaginary *= -1;
+}
