190201571a226fdbee8e425c9884e67e5e9be5b9
ShahJabeen (ShahJabeen) Sajid <sajs4110@36loki.cs.uleth.ca>
2018-11-15 13:44:55 -0700

    New Changes :p

--- src/ComplexNumber.cc
+++ src/ComplexNumber.cc
@@ -1,1 +1,33 @@
 #include "ComplexNumber.hh"
+
+
+using namespace ComplexSpace;
+
+ComplexNumber::ComplexNumber(float r, float i)
+{
+   real = r;
+   imaginary= i;
+   exponent =0;
+}
+
+
+ComplexNumber::~ComplexNumber()
+{
+}
+
+ComplexNumber ComplexNumber::Add(ComplexNumber x)
+{
+   ComplexNumber a;
+   a.real= x.real + real;
+   a.imaginary= x.imaginary + imaginary;
+   return a;
+}
+ComplexNumber ComplexNumber::Product(ComplexNumber x)
+{
+   ComplexNumber a;
+   a.real= x.real * real;
+   a.imaginary= x.imaginary * imaginary * -1;
+   return a;
+}
+
+
