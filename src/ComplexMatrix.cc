0ad4e67bece96b777e931961ae4cc9fd5650a4cd
Daylend <drkcelestial@gmail.com>
2018-11-15 15:55:27 -0700

    Fixed ComplexMatrix Add

--- src/ComplexMatrix.cc
+++ src/ComplexMatrix.cc
@@ -6,9 +6,9 @@ using namespace std;
 ComplexMatrix ComplexMatrix::Add(ComplexMatrix A)
 {
     vector<ComplexVector> newMatrix = vector<ComplexVector>();
-    for(int i = 0; i < Matrix.size; i++)
+    for(int i = 0; i < v.size; i++)
     {
-        ComplexVector added = Matrix[i].Add(A.Matrix[i]);
+        ComplexVector added = v[i].Add(A.v[i]);
         newMatrix.push_back(added);
     }
 }
